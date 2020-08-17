afd8971248c4d864bf6fba2614e9f5c8a374bdd7
Emanuel Massafera <emanuel301@live.com>
2020-08-17 15:43:26 -0300

    Bloqueio e redirecionamento de rotas

--- 10-controle-da-sessao-de-usuario/controle-da-sessao-de-usuario/src/routes/users.js
+++ 10-controle-da-sessao-de-usuario/controle-da-sessao-de-usuario/src/routes/users.js
@@ -4,11 +4,12 @@ const SessionController = require("../app/controllers/SessionController");
 const UserController = require("../app/controllers/UserController");
 const UserValidator = require("../app/validators/user");
 const SessionValidator = require("../app/validators/session");
+const { isLoggedRedirectToUsers, onlyUsers } = require("../app/middlewares/session")
 
 
 // USERS
 
-routes.get("/login", SessionController.loginForm);
+routes.get("/login", isLoggedRedirectToUsers, SessionController.loginForm);
 routes.post("/login", SessionValidator.login, SessionController.login);
 routes.post("/logout", SessionController.logout);
 
@@ -20,7 +21,7 @@ routes.post("/logout", SessionController.logout);
 routes.get("/register", UserController.registerForm);
 routes.post("/register", UserValidator.post, UserController.post);
 
-routes.get("/", UserValidator.show, UserController.show);
+routes.get("/", onlyUsers, UserValidator.show, UserController.show);
 routes.put("/", UserValidator.update, UserController.update);
 // routes.delete("/", UserController.delete);
 
