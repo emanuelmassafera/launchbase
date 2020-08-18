da346c5e7d31a480be22d303020315fb47022733
Emanuel Massafera <emanuel301@live.com>
2020-08-18 15:22:36 -0300

    Recuperação de senha

--- 10-controle-da-sessao-de-usuario/controle-da-sessao-de-usuario/src/app/validators/session.js
+++ 10-controle-da-sessao-de-usuario/controle-da-sessao-de-usuario/src/app/validators/session.js
@@ -12,7 +12,7 @@ async function login(req, res, next) {
 
     const passed = await compare(password, user.password);
 
-    if(!passed) {
+    if (!passed) {
         return res.render("session/login", {
             user: req.body,
             error: "Senha incorreta!"
@@ -24,6 +24,73 @@ async function login(req, res, next) {
     next();
 }
 
+async function forgot(req, res, next) {
+    const { email } = req.body;
+
+    try {
+        let user = await User.findOne({ where: { email } });
+
+        if (!user) return res.render("session/forgot-password", {
+            user: req.body,
+            error: "Email não cadastrado!"
+        });
+
+        req.user = user;
+
+        next();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function reset(req, res, next) {
+    const { email, password, token, passwordRepeat } = req.body;
+
+    // procurar usuário
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) return res.render("session/password-reset", {
+        user: req.body,
+        token,
+        error: "Usuário não cadastrado!"
+    });
+
+    // verificar se as senhas coincidem
+    if (password != passwordRepeat) {
+        return res.render("session/password-reset", {
+            user: req.body,
+            token,
+            error: "As senhas não conferem."
+        });
+    }
+
+    // verificar se o token é válido
+    if (token != user.reset_token) {
+        return res.render("session/password-reset", {
+            user: req.body,
+            token,
+            error: "Token inválido! Solicite uma nova recuperação de senha."
+        });
+    }
+
+    // verificar se o token expirou
+    let now = new Date();
+    now = now.setHours(now.getHours());
+
+    if(now > user.reset_token_expires) {
+        return res.render("session/password-reset", {
+            user: req.body,
+            token,
+            error: "Token expirado. Por favor, solicite uma nova recuperação de senha."
+        });
+    }
+
+    req.user = user;
+    next();
+}
+
 module.exports = {
-    login
+    login,
+    forgot,
+    reset
 }
--- 10-controle-da-sessao-de-usuario/controle-da-sessao-de-usuario/src/routes/users.js
+++ 10-controle-da-sessao-de-usuario/controle-da-sessao-de-usuario/src/routes/users.js
@@ -13,10 +13,10 @@ routes.get("/login", isLoggedRedirectToUsers, SessionController.loginForm);
 routes.post("/login", SessionValidator.login, SessionController.login);
 routes.post("/logout", SessionController.logout);
 
-// routes.get("/forgot-password", SessionController.forgotForm);
-// routes.get("/password-reset", SessionController.resetForm);
-// routes.post("/forgot-password", SessionController.forgot);
-// routes.post("/password-reset", SessionController.reset);
+routes.get("/forgot-password", SessionController.forgotForm);
+routes.get("/password-reset", SessionController.resetForm);
+routes.post("/forgot-password", SessionValidator.forgot, SessionController.forgot);
+routes.post("/password-reset", SessionValidator.reset, SessionController.reset);
 
 routes.get("/register", UserController.registerForm);
 routes.post("/register", UserValidator.post, UserController.post);
