224236cd62bd51b1995286d622dc6498c6e76cb5
Emanuel Massafera <emanuel301@live.com>
2021-01-08 11:31:09 -0300

    Dados do Pedido

--- 12-funcionalidades-extras-para-a-launchstore/funcionalidades-extras-para-a-launchstore/src/app/controllers/OrderController.js
+++ 12-funcionalidades-extras-para-a-launchstore/funcionalidades-extras-para-a-launchstore/src/app/controllers/OrderController.js
@@ -4,6 +4,7 @@ const Order = require('../models/Order');
 
 const mailer = require('../../lib/mailer');
 const Cart = require('../../lib/cart');
+const { formatPrice, date } = require('../../lib/utils');
 
 const email = (seller, product, buyer) => `
 <h2>Olá, ${seller.name}</h2>
@@ -23,6 +24,54 @@ const email = (seller, product, buyer) => `
 `;
 
 module.exports = {
+  async index(req, res) {
+    // Pegar os pedidos do usuário
+    let orders = await Order.findAll({
+      where: { buyer_id: req.session.userId },
+    });
+
+    const ordersPromise = orders.map(async (order) => {
+      // Detalhes do produto
+      order.product = await LoadProductService.load('products', {
+        where: { id: order.product_id },
+      });
+
+      // Detalhes do comprador
+      order.buyer = await User.findOne({
+        where: { id: order.buyer_id },
+      });
+
+      // Detalhes do vendedor
+      order.seller = await User.findOne({
+        where: { id: order.seller_id },
+      });
+
+      // Formatação de preço
+      order.formattedPrice = formatPrice(order.price);
+      order.formattedTotal = formatPrice(order.total);
+
+      // Formatação do status
+      const status = {
+        open: 'Aberto',
+        sold: 'Vendido',
+        canceled: 'Cancelado',
+      };
+
+      order.formattedStatus = status[order.status];
+
+      // Formatação de atualizado
+      const updatedAt = date(order.updated_at);
+
+      order.formattedUpdatedAt = `${order.formattedStatus} em ${updatedAt.day}/${updatedAt.month}/${updatedAt.year} às ${updatedAt.hour}h${updatedAt.minutes}`;
+
+      return order;
+    });
+
+    orders = await Promise.all(ordersPromise);
+
+    return res.render('orders/index', { orders });
+  },
+
   async post(req, res) {
     try {
       // Pegar os produtos do carrinho
@@ -69,6 +118,10 @@ module.exports = {
       });
 
       await Promise.all(createOrdersPromise);
+
+      // Limpar o carrinho
+      delete req.session.cart;
+      Cart.init();
 
       return res.render('orders/success');
     } catch (error) {
--- 12-funcionalidades-extras-para-a-launchstore/funcionalidades-extras-para-a-launchstore/src/routes/orders.js
+++ 12-funcionalidades-extras-para-a-launchstore/funcionalidades-extras-para-a-launchstore/src/routes/orders.js
@@ -5,5 +5,6 @@ const OrderController = require('../app/controllers/OrderController');
 const { onlyUsers } = require('../app/middlewares/session');
 
 routes.post('/', onlyUsers, OrderController.post);
+routes.get('/', onlyUsers, OrderController.index);
 
 module.exports = routes;
